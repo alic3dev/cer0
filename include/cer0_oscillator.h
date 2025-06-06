05b4db3962b2bcb501ed26615894926017fbe2e7
alic3 <alic3dev@gmail.com>
2025-06-05 19:42:39 -0700

    `cer0_oscillator`:implement

---
+++ include/cer0_oscillator.h
@@ -0,0 +1,40 @@
+#ifndef __cer0_oscillator_h
+#define __cer0_oscillator_h
+
+#import <cer0_phase.h>
+#import <cer0_signal.h>
+
+struct cer0_oscillator {
+  float amplitude;
+  struct cer0_phase phase;
+  enum cer0_signal signal;
+  cer0_signal_function signal_function;
+};
+
+void cer0_oscillator_initialize(
+  struct cer0_oscillator*,
+  float,
+  float,
+  enum cer0_signal
+);
+
+void cer0_oscillator_frequency_set(
+  struct cer0_oscillator*,
+  float
+);
+
+void cer0_oscillator_sample_rate_set(
+  struct cer0_oscillator*,
+  float
+);
+
+void cer0_oscillator_signal_set(
+  struct cer0_oscillator*,
+  enum cer0_signal
+);
+
+float cer0_oscillator_poll(
+  struct cer0_oscillator*
+);
+
+#endif
