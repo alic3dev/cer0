e63fc2573a881a36269ebcbb7d78324a8fd7b2c2
alic3 <alic3dev@gmail.com>
2025-06-05 19:42:21 -0700

    `cer0_phase`:add->{`struct cer0_phase`}.with_functionality

--- include/cer0_phase.h
+++ include/cer0_phase.h
@@ -1,7 +1,45 @@
 #ifndef __cer0_phase_h
 #define __cer0_phase_h
 
-float cer0_phase_get_increment(float, float);
-float cer0_phase_advance(float, float);
+struct cer0_phase {
+  float frequency;
+  float increment;
+  float sample_rate;
+  float value;
+};
+
+void cer0_phase_initialize(
+  struct cer0_phase*,
+  float,
+  float
+);
+
+void cer0_phase_poll(
+  struct cer0_phase*
+);
+
+void cer0_phase_frequency_set(
+  struct cer0_phase*,
+  float
+);
+
+void cer0_phase_sample_rate_set(
+  struct cer0_phase*,
+  float
+);
+
+void cer0_phase_increment_set(
+  struct cer0_phase*
+);
+
+float cer0_phase_get_increment(
+  float,
+  float
+);
+
+float cer0_phase_advance(
+  float,
+  float
+);
 
 #endif
--- sources/cer0_phase.c
+++ sources/cer0_phase.c
@@ -1,6 +1,55 @@
 #include <cer0_phase.h>
 #include <cer0_constants.h>
 
+void cer0_phase_initialize(
+  struct cer0_phase* phase,
+  float sample_rate,
+  float frequency
+) {
+  phase->frequency = frequency;
+  phase->sample_rate = sample_rate;
+
+  cer0_phase_increment_set(phase);
+
+  phase->value = 0;
+}
+
+void cer0_phase_poll(
+  struct cer0_phase* phase
+) {
+  phase->value = cer0_phase_advance(
+    phase->value,
+    phase->increment
+  );
+}
+
+void cer0_phase_frequency_set(
+  struct cer0_phase* phase,
+  float frequency
+) {
+  phase->frequency = frequency;
+
+  cer0_phase_increment_set(phase);
+}
+
+void cer0_phase_sample_rate_set(
+  struct cer0_phase* phase,
+  float sample_rate
+) {
+  phase->sample_rate = sample_rate;
+
+  cer0_phase_increment_set(phase);
+}
+
+void cer0_phase_increment_set(
+  struct cer0_phase* phase
+) {
+  phase->increment = cer0_phase_get_increment(
+    phase->sample_rate,
+    phase->frequency
+  );
+}
+
 float cer0_phase_get_increment(
   float sample_rate,
   float frequency
